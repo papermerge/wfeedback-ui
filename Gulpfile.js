c9ead519e436dcaee813815ada5b9baeebde4a12
Eugen Ciur <eugen@papermerge.com>
2021-03-14 08:33:57 +0100

    trying to setup project with gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,42 @@
-function hello(callback) {
-  console.log('Hello Zell');
-  callback();
+const {src, dest, watch, series, parallel} = require('gulp');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const replace = require('gulp-replace');
+const sourcemaps = require('gulp-sourcemaps');
+
+const files = {
+    sass_path: "app/scss/**/*.scss",
+    js_path: "app/js/**/*.js"
 };
 
-exports.default = hello;
+function sass_task() {
+    return src(files.sass_path)
+        .pipe(sass())
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('dist'));
+}
+
+function js_task() {
+    return src(files.js_path)
+        .pipe(concat('all.js'))
+        .pipe(dest('dist'));
+}
+
+function cache_bust_task() {
+    return src(['index.html'])
+    .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
+    .pipe(dest('.'))
+}
+
+function watch_task() {
+    watch(
+        [files.scss_path, files.js_path],
+        parallel(sass_task, js_task)
+    )
+}
+
+exports.default = series(
+    parallel(sass_task, js_task),
+    cache_bust_task,
+    watch_task
+);
--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 Workers' feedback user interface.
 Provides (user interface) feedback for various background tasks like document/page OCR.
+
+
+# Usage
+
+
+$ gulp
